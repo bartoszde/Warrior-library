927dcb5cf80f9d99f93714b3ca2457c52daf4db1
Bartosz Dubinski <bartas.de@gmail.com>
2023-03-20 15:11:10 +0100

    create book functionality

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,17 +1,18 @@
-module.exports = (app) => {
-  app.use((req, res, next) => {
-    // this middleware runs whenever requested page is not available
-    res.status(404).render("not-found");
-  });
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
 
-  app.use((err, req, res, next) => {
-    // whenever you call next(err), this middleware will handle the error
-    // always logs the error
-    console.error("ERROR", req.method, req.path, err);
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-    // only render if the error ocurred before sending the response
-    if (!res.headersSent) {
-      res.status(500).render("error");
-    }
+const MONGO_URI =  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/library-project";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const databaseName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
   });
-};
---
+++ routes/book.routes.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const Book = require('../models/Book.model');
+const router = express.Router();
+
+// GET /books
+router.get("/books", (req, res, next) => {
+
+  Book.find()
+    .then(booksArr => {
+
+      const data = {
+        books: booksArr
+      };
+
+      res.render("books/books-list", data);
+    })
+    .catch(e => {
+      console.log("error getting books from DB", e);
+      next(e);
+    });
+});
+
+
+
+//GET /books/create
+router.get("/books/create", (req, res, next) => {
+  res.render("books/book-create");
+});
+
+
+
+//POST /books
+router.post("/books", (req, res, next) => {
+  
+  const bookDetails = {
+    title: req.body.title,
+    description: req.body.description,
+    author: req.body.author,
+    rating: req.body.rating
+  }
+  Book.create(bookDetails)
+    .then( bookFromDB => {
+      res.send("looking good.... Alex, I think we were able to create your book");
+    })
+    .catch(e => {
+      console.log("error creating new book", e);
+      next(e);
+    });
+
+
+});
+
+//GET /books/:bookId
+router.get("/books/:bookId", (req, res, next) => {
+
+  const {bookId} = req.params;
+
+  Book.findById(bookId)
+    .then( bookDetails => {
+      res.render("books/book-details", bookDetails);
+    })
+    .catch(e => {
+      console.log("error getting book details from DB", e);
+      next(e);
+    });
+
+});
+
+
+
+
+
+
+
+module.exports = router;
